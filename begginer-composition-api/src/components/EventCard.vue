6f0ba0209e92a3b7cde1076f66a3805e50317e07
olha.codes <57961694+olhacodes@users.noreply.github.com>
2024-05-11 17:20:23 +0300

    added service to get event by id

--- begginer-composition-api/src/components/EventCard.vue
+++ begginer-composition-api/src/components/EventCard.vue
@@ -8,10 +8,12 @@ defineProps({
 </script>
 
 <template>
+  <RouterLink to="/event/123">
   <div class="event-card">
     <h2>{{ event.title }}</h2>
     <span>@ {{ event.time }} on {{ event.date }}</span>
   </div>
+</RouterLink> 
 </template>
 
 <style scoped>
