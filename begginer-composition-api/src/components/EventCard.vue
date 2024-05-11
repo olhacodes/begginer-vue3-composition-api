942c62d77fcb21feb88048e77090600bb11d325d
olha.codes <57961694+olhacodes@users.noreply.github.com>
2024-05-11 15:47:41 +0300

    added data to event component

--- begginer-composition-api/src/components/EventCard.vue
+++ begginer-composition-api/src/components/EventCard.vue
@@ -1,15 +1,28 @@
 <script setup>
+import { ref } from 'vue';
 // defineProps({
 //   msg: {
 //     type: String,
 //     required: true
 //   }
 // })
+const event = ref({
+  id: 14353,
+  title: 'Vue.js Workshop',
+  date: '2021-09-01',
+  time: '10:00',
+  location: 'Online',
+  description: 'Join us for a full day of Vue.js training and fun!',
+  category: 'workshop',
+  organizer: 'Vue School',
+  freeEnterance: true
+})
 </script>
 
 <template>
   <div class="event-card">
-    
+    <h2>{{ event.title }}</h2>
+    <span>@ {{ event.time }} on {{ event.date }}</span>
   </div>
 </template>
 
